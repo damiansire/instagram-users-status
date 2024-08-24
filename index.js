b601b36846d20b0892cdadcbf47c1c5ea0586229
Damian Sire <damiansire@gmail.com>
2024-08-23 21:19:20 -0300

    feat: add base structure for get profile data and modularize the code

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const axios = require("axios");
 
+async function getUserStatus(username) {
+  try {
+    console.log("Verifying user:", username);
+    const response = await axios.get(`https://www.instagram.com/${username}/`);
+
+    if (response.data.includes(`name="description"`)) {
+      return { username, followers: 0, following: 0 };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error al verificar", username, error);
+  }
+}
+
 async function getInstagramUsersStatus(usernames) {
   const activeUsernames = [];
   const inactiveUsernames = [];
   const activeUsernamesStatus = [];
   for (const username of usernames) {
     try {
-      const response = await axios.get(
-        `https://www.instagram.com/${username}/`
-      );
+      const userStatus = await getUserStatus(username);
 
-      if (response.data.includes(`name="description"`)) {
-        activeUsernames.push(username);
+      if (userStatus) {
+        activeUsernames.push(userStatus.username);
+        activeUsernamesStatus.push(userStatus);
       } else {
         inactiveUsernames.push(username);
       }
